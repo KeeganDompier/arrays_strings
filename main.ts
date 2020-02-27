0cf9c5c874314a4e33c25aa5ad597d840c8841ea
KeeganDompier <58610698+KeeganDompier@users.noreply.github.com>
2020-02-27 13:25:24 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let food1 = "burrito"
+let food2 = "apple"
+let food3 = "avacado"
+let food4 = "pear"
+let food5 = "chicken"
+
+let listFoods = ["burrito", "apple", "avacado", "pear", "chicken"]
+game.splash("This is my first favorite food: " + listFoods[0])
+game.splash("This is my third favorite food: " + listFoods[2])
+game.splash("This is my fith favorite food: " + listFoods[4])
